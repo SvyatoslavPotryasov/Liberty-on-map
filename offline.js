337bb870d1931ea10e14eb69867735eeedad2545
SvyatoslavPotryasov <96661558+SvyatoslavPotryasov@users.noreply.github.com>
2022-06-01 21:14:30 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1654107179,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/Игрок1-sheet0.png",
+		"images/Игрок1-sheet1.png",
+		"images/игрок2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"firebase.js",
+		"cordova.oauth2.js"
+	]
+}
